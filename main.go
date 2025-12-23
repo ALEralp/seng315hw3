bcd1806cc3d690a4a7e0710aecf25cdc0b4a202e
ALEralp <157644784+ALEralp@users.noreply.github.com>
2025-12-23 23:02:40 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -6,26 +6,24 @@ import (
 )
 
 func main() {
-	// Simple CLI args for nickname
-	nick := "User"
+	defaultNick := "User"
 	if len(os.Args) > 1 {
-		nick = os.Args[1]
+		defaultNick = os.Args[1]
 	}
 
-	node, err := NewP2PNode(nick)
+	node, err := NewP2PNode(defaultNick)
 	if err != nil {
 		fmt.Printf("Error creating P2P node: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Starting P2P Node...")
+	fmt.Printf("Starting P2P Node (UUID: %s)...\n", node.StableID)
 	if err := node.Start(); err != nil {
 		fmt.Printf("Error starting P2P services: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Auto-join global chat
-	node.JoinChannel("global-chat")
+	node.JoinChannel("global-chat", "")
 
 	gui := NewGUI(node)
 	gui.Run()
